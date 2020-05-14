8488e53a89ce4ead6e1c76e3f371586dbc82681b
thattan79 <65060492+thattan79@users.noreply.github.com>
2020-05-14 20:39:17 +0530

    Update Calculator.java
    test

--- src/main/java/com/Calculator.java
+++ src/main/java/com/Calculator.java
@@ -2,7 +2,7 @@ package com;
 
 public class Calculator {
     public void sum(int a,  int b){
-        System.out.println("SUm is--sdasdasdfsdf"+a+b);
+        System.out.println("SUm is--"+a+b);
     }
 }
 
