d07b2ffaf104d82de4f9d3f928c382577d1b24bf
Dileep T Paul <dileeptpaul2019@gmail.com>
2020-05-14 19:32:58 +0530

    adadasdasdadadadasd

---
+++ src/main/java/com/Scalculator.java
@@ -0,0 +1,8 @@
+package com;
+
+public class Scalculator {
+    public void sum(int a,  int b){
+        System.out.println("SUm is--sdasdasdfsdf"+a+b);
+    }
+}
+
---
+++ src/test/java/com/ScalculatorTest.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ScalculatorTest {
+
+    @Test
+    public void sum() {
+    }
+}
